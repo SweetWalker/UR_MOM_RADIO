5b6dc76eba12bc2ebb69c47810130e7b7a8fc85c
Zoilorys <zoilorys@gmail.com>
2015-09-18 19:33:44 +0300

    ALALALAL

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-var http = require('http'),
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
     fs   = require('fs'),
     filePath = 'audio/disco.mp3',
     io = require('socket.io')(http),
     stat = fs.statSync(filePath);
 
-http.createServer(function(request, response) {
-    response.write('/frontend/index.html');
-    response.end();
-}).listen(2000, function() {
+app.get('/', express.static(__dirname + "/frontend/"));
+
+http.listen(2000, function() {
     console.log('UR MOM running on port 2000');
 });
 
 io.on('connection', function(socket) {
     console.log('some shithead connected!');
+
+    stream = fs.createReadStream(__dirname + "/" + filePath);
+
+    stream.on('data', function(chunk) {
+        socket.broadcast.emit('audio', { data: chunk });
+    });
+
+    stream.on('finish', function() {
+        console.log("LOG: stream ended");
+    });
 
     socket.on('disconnect', function() {
         console.log('HE IS RUNNING AWAY!!!');
