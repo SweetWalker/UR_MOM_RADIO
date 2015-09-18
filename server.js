4dbf5780c802b02bacc2cac07874d48b26403375
SweetWalker <cactusjoe666@gmail.com>
2015-09-18 18:35:35 +0300

    utried.jpg

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http'),
+    fs   = require('fs'),
+    filePath = 'disco.mp3',
+    stat = fs.statSync(filePath);
+
+http.createServer(function(request, response) {
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+    var somefunc = function () {
+    var pew = fs.createReadStream(filePath)
+    pew.on('open', function(){
+    	pew.pipe(response);
+    	console.log('First playing')
+    })
+    pew.on('end', function(){
+    	process.nextTick( function(){
+    		pew.pipe(response)
+    	});
+    	console.log('END')
+    })
+}
+
+somefunc();
+
+
+})
+.listen(2000);
+
